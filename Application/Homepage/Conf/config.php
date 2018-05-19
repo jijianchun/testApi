9bb403d4261129a6ddc0192ef5dfb03439f8831a
健男-春 <549332732@qq.com>
2018-05-19 23:38:57 +0800

    新增模块

---
+++ Application/Homepage/Conf/config.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+  'DB_NAME'                => 'homepage', // 数据库名
+  'DB_PREFIX'              => 'hp_' // 数据库表前缀
+);
---
+++ Application/Homepage/Controller/IndexController.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Homepage\Controller;
+header('Access-Control-Allow-Origin:*');
+header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, DELETE");
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+ 	exit;
+}
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        echo 'homepage';
+    }
+}
