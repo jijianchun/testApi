1cd6bfda6eaf28441fb0ec5580fb724dd67fd4a5
jijianchun <jijianchun@kedacom.com>
2018-05-21 10:52:08 +0800

    新增保存留言功能

--- Application/Homepage/Controller/IndexController.class.php
+++ Application/Homepage/Controller/IndexController.class.php
@@ -11,13 +11,35 @@ use Think\Controller;
 
 class IndexController extends Controller
 {
+    protected $comment_model;
+    public function _initialize() {
+
+      $this->comment_model = M('comments');
+    }
+
     public function index()
     {
         echo 'homepage';
     }
 
-    // 提交留言
-    public funtion addComment () {
+    // 提交保存留言
+    public function addComment () {
+      if (IS_POST) {
+  			// $req_data = json_decode($GLOBALS['HTTP_RAW_POST_DATA'],true);
+  			// $data['name'] = $req_data['name'];
+  			// $data['email'] = $req_data['email'];
+  			// $data['content'] = $req_data['content'];
+        // $data['time'] = time();
 
+        $data['name'] = $_POST['name'];
+  			$data['email'] = $_POST['email'];
+  			$data['content'] = $_POST['content'];
+        $data['time'] = date('Y-m-d H:i:s', time());
+  			if($this->comment_model->add($data) !== false){
+  				echo json_encode(array('status'=>1,'msg'=>'添加成功'));
+  			} else {
+  				echo json_encode(array('status'=>0,'msg'=>'添加失败'));
+  			}
+  		}
     }
 }
