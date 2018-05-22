d8060e715323a6f2572fe839abf9439bcfb2f429
jijianchun <jijianchun@kedacom.com>
2018-05-22 18:08:53 +0800

    跨域问题

--- Application/Homepage/Controller/IndexController.class.php
+++ Application/Homepage/Controller/IndexController.class.php
@@ -1,6 +1,6 @@
 <?php
 namespace Homepage\Controller;
-header('Access-Control-Allow-Origin:http://www.jijianchun.cn');
+header('Access-Control-Allow-Origin:*');
 header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, DELETE");
 header('Access-Control-Allow-Headers:x-requested-with,content-type');
 if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
