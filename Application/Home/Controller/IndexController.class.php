fb952662dba72e3717688068f5cedd872d6f4af9
jijianchun <jijianchun@kedacom.com>
2018-07-03 16:58:43 +0800

    增加大鹏采集内容

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,6 +1,12 @@
 <?php
 namespace Home\Controller;
-header("Content-Type:text/html; charset=utf-8");
+header('Access-Control-Allow-Origin:*');
+header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, DELETE");
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+ 	exit;
+}
+// header("Content-Type:text/html; charset=utf-8");
 
 use Think\Controller;
 
@@ -10,5 +16,107 @@ class IndexController extends Controller
     {
         echo 'hello world';
     }
-    
+
+    // element-tree测试
+    public function hello() {
+      // http_response_code(201);
+
+      $data = array();
+      for($i=1;$i<=2;$i++) {
+      	$data[] = array(
+      		'id' => $i,
+      		'label' => '一级 '.$i
+      	);
+      }
+
+      $data2 = array();
+      for($i=3;$i<=5;$i++) {
+      	$data2[] = array(
+      		'id' => $i,
+      		'label' => '二级 '.($i-2)
+      	);
+      }
+
+      $data3 = array();
+      for($i=6;$i<=1000;$i++) {
+      	$data3[] = array(
+      		'id' => $i,
+      		'label' => '三级 '.($i-5)
+      	);
+      }
+
+      foreach($data as $key=> $value) {
+      	if ($value['id'] == 1) {
+      		$data[$key]['children'] = $data2;
+      	}
+      }
+
+      foreach($data as $key => $value) {
+      	foreach($value['children'] as $k=>$v) {
+	  		if ($v['id'] == 3) {
+	  			$data[$key]['children'][$k]['children'] = $data3;
+	  		}
+	  	}
+      }
+      
+      echo json_encode($data);
+    }
+
+    // iview-tree测试
+    public function hello2() {
+    	$data2 = array();
+    	for($i=1;$i<=2;$i++) {
+	      	$data2[] = array(
+	      		'title' => 'parent 1-'.$i,
+	      		'expand' => true,
+	      	);
+	    }
+
+    	$data = array(
+    		array(
+    			'title' => 'parent 1',
+    			'expand' => true,
+    			'children' => $data2
+    		)
+    	);
+
+    	$data3 = array();
+    	for($i=1;$i<=1000;$i++) {
+    		$data3[] = array(
+    			'title' => 'leaf 1-1-'. $i
+    		);
+    	}
+
+    	foreach($data[0]['children'] as $key => $value) {
+    		if ($value['title'] == 'parent 1-1') {
+    			$data[0]['children'][$key]['children'] = $data3;
+    		}
+    	}
+
+    	echo json_encode($data);
+    }
+
+    // 大鹏采集
+    public function collect() {
+      $url = $_GET['urlName'];
+      if (!$url) {
+        echo json_encode(array());
+        exit;
+      }
+      $meta_tags = get_meta_tags($url);
+      $str = $meta_tags['keywords'];
+      $rule = "/High Quality (.*)/";
+      preg_match($rule, $str, $results);
+      $arr = explode(',', $results[1]);
+
+      $output_arr = array();
+      foreach($arr as $key => $value) {
+        $output_arr[] = array(
+          'index' => $key + 1,
+          'name' => $value
+        );
+      }
+      echo json_encode($output_arr);
+    }
+
 }
